256c19c7a17f99b0b9e01a96584c140fba2b8837
PaulaRobertaSuelen <paularobertasuelen27@gmail.com>
2025-03-31 14:15:44 -0300

    fix: corrigindo um erro de importação

--- To-do/src/components/index.js
+++ To-do/src/components/index.js
@@ -1,11 +1,19 @@
 import Title from "./Title";
 import SubTitle from "./SubTitle";
 import Footer from "./Footer";
-import Container from "./container";
 import CardList from "./CardList";
 import Card from "./Card";
 import FinishedTasks from "./FinishedTasks";
 import NewTaskForm from "./NewTaskForm";
+import Container from "./Container";
 
-
-export{Title, SubTitle, Footer, CardList, Card, FinishedTasks, NewTaskForm, Container};
+export {
+  Title,
+  SubTitle,
+  Footer,
+  CardList,
+  Card,
+  FinishedTasks,
+  NewTaskForm,
+  Container,
+};
